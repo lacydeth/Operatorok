211ad46f1437823405bc7e49145593ebdddd73ff
white <white@DESKTOP-D1M2GIS>
2023-05-21 17:50:22 +0200

    mentes

--- Operatorok/Program.cs
+++ Operatorok/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System.Collections.Immutable;
+using System.Text;
+using Operatorok;
+
+List<operatorok> operatorok = new List<operatorok>();
+
+
+StreamReader sr = new StreamReader("kifejezesek.txt");
+sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    string[] adat = sr.ReadLine().Split();
+    operatorok.Add(new operatorok(int.Parse(adat[0]), adat[1], int.Parse(adat[2])));
+}
+sr.Close();
+
+Console.WriteLine($"2. feladat: {operatorok.Count()}");
+
+Console.WriteLine($"3. feladat: {operatorok.Count(x => x.OpJel == "mod")}");
+
+Console.WriteLine($"4. feladat: {(operatorok.First(x => x.OpElso % 10 == 0 && x.OpMas % 10 == 0) != null ? "Van" : "Nincs")}");
+
+Console.WriteLine("5. feladat: Statisztika");
+Console.WriteLine($"mod -> {operatorok.Count(x => x.OpJel == "mod")} db");
+Console.WriteLine($"/ -> {operatorok.Count(x => x.OpJel == "/")} db");
+Console.WriteLine($"div -> {operatorok.Count(x => x.OpJel == "div")} db");
+Console.WriteLine($"- -> {operatorok.Count(x => x.OpJel == "-")} db");
+Console.WriteLine($"* -> {operatorok.Count(x => x.OpJel == "*")} db");
+Console.WriteLine($"+ -> {operatorok.Count(x => x.OpJel == "+")} db");
+
---
+++ Operatorok/operatorok.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatorok
+{
+    internal class operatorok
+    {
+        int opElso;
+        string opJel;
+        int opMas;
+
+        public operatorok(int opElso, string opJel, int opMas)
+        {
+            this.opElso = opElso;
+            this.opJel = opJel;
+            this.opMas = opMas;
+        }
+
+        public int OpElso { get => opElso; }
+        public string OpJel { get => opJel; }
+        public int OpMas { get => opMas; }
+    }
+}
